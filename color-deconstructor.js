aa2d012be81ab95cc51a9fa151424ec0476610cb
Patrick Navarre <patrick.navarre@codeimmersives.com>
2020-10-01 11:33:04 -0400

    finished color-deconstructor

--- color-deconstructor.js
+++ color-deconstructor.js
@@ -2,9 +2,17 @@
  * YOUR CODE HERE *
  ******************/
 
+function colorDeconstructor(secondary){
+  if (secondary === 'orange') {
+    return 'red and yellow';
+  } else if (secondary === 'purple') {
+    return 'red and blue';
+  } else if (secondary === 'green') {
+    return 'blue and yellow'
+  }
+}
 
-
-
+q
 // Our setup code here. Don't touch!
 if(typeof colorDeconstructor === 'undefined') {
   colorDeconstructor = undefined;
