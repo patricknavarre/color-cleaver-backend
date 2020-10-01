9b3ffc788b666a5203d7a2d175c0a8af9a264523
Patrick Navarre <patrick.navarre@codeimmersives.com>
2020-09-30 22:02:30 -0400

    cleaned up code

--- is-valid-primary.js
+++ is-valid-primary.js
@@ -2,16 +2,10 @@
  * YOUR CODE HERE *
  ******************/
 
-function isValidPrimary(primary1, primary2, primary3){
-  if (primary1 === 'red' || primary1 === 'yellow' || primary1 === 'blue'){
+function isValidPrimary(primary){
+  if (primary === 'red' || primary === 'yellow' || primary === 'blue'){
     return true
     }  
-  if (primary2 === 'red' || primary2 === 'yellow' || primary2 === 'blue'){
-    return true
-  }
-  if (primary3 === 'red' || primary3 ===  'yellow' || primary3 === 'blue'){
-    return true
-  }
     else {
       return false
     }
