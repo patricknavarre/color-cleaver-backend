6e8c7fc5be0769a1b28107b8fc5d0a679e05320f
Patrick Navarre <patrick.navarre@codeimmersives.com>
2020-09-30 22:03:22 -0400

    cleaned up code

--- is-valid-secondary.js
+++ is-valid-secondary.js
@@ -6,12 +6,6 @@ function isValidSecondary(secondary1, secondary2, secondary3){
   if (secondary1 === 'orange' || secondary1 === 'purple' || secondary1 === 'green'){
     return true
     }  
-  if (secondary2 === 'orange' || secondary2 === 'purple' || secondary2 === 'green'){
-    return true
-  }
-  if (secondary3 === 'orange' || secondary3 ===  'purple' || secondary3 === 'greenr'){
-    return true
-  }
     else {
       return false
     }
