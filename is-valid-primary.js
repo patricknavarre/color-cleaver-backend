e6edcd69052719fe2d0ac7b9a2862ba0a84439a5
Patrick Navarre <patrick.navarre@codeimmersives.com>
2020-09-30 21:26:59 -0400

    isValidPrimary Function Completed

--- is-valid-primary.js
+++ is-valid-primary.js
@@ -2,6 +2,21 @@
  * YOUR CODE HERE *
  ******************/
 
+function isValidPrimary(primary1, primary2, primary3){
+  if (primary1 === 'red' || primary1 === 'yellow' || primary1 === 'blue'){
+    return true
+    }  
+  if (primary2 === 'red' || primary2 === 'yellow' || primary2 === 'blue'){
+    return true
+  }
+  if (primary3 === 'red' || primary3 ===  'yellow' || primary3 === 'blue'){
+    return true
+  }
+    else {
+      return false
+    }
+}
+
 
 
 
