b5a20717580c90e3bee304075cedd6d5fab8df4e
Patrick Navarre <patrick.navarre@codeimmersives.com>
2020-09-30 21:37:11 -0400

    completed isValidSecondary function

--- is-valid-secondary.js
+++ is-valid-secondary.js
@@ -2,7 +2,20 @@
  * YOUR CODE HERE *
  ******************/
 
-
+function isValidSecondary(secondary1, secondary2, secondary3){
+  if (secondary1 === 'orange' || secondary1 === 'purple' || secondary1 === 'green'){
+    return true
+    }  
+  if (secondary2 === 'orange' || secondary2 === 'purple' || secondary2 === 'green'){
+    return true
+  }
+  if (secondary3 === 'orange' || secondary3 ===  'purple' || secondary3 === 'greenr'){
+    return true
+  }
+    else {
+      return false
+    }
+}
 
 
 // Our setup code here. Don't touch!
